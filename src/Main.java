05ad7aef6ebe20a9aa1a699dae5b631da75e13b6
norki <norkink89@mail.ru>
2023-01-14 19:33:05 +0400

    Objects completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+        Author joanRowling = new Author("Joan","Rowling");
+        Author konstantinSimonov = new Author("Konstantin","Simonov");
+        Book harryPotter = new Book(joanRowling,"Harry Potter",2001);
+        Book alivesAndDeads = new Book(konstantinSimonov, "Живые и мертвые", 1959);
+        harryPotter.setPublishingYear(1999);
+        alivesAndDeads.setPublishingYear(1970);
+        System.out.println("First Book of collection: " + harryPotter.getTitle() + " by " + harryPotter.getAuthor().getAuthorName()+ " " + harryPotter.getAuthor().getAuthorSurname()+ " published in "+ harryPotter.getPublishingYear());
+        System.out.println("Second Book of collection: " + alivesAndDeads.getTitle() + " by " + alivesAndDeads.getAuthor().getAuthorName()+" " + alivesAndDeads.getAuthor().getAuthorSurname()+ " published in "+ alivesAndDeads.getPublishingYear());
+          }
+    
 }
