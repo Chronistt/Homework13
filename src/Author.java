a592f08787a78bb412f90238822e68260e46e2c0
norki <norkink89@mail.ru>
2023-01-15 11:44:19 +0400

    Methods of objects

--- src/Author.java
+++ src/Author.java
@@ -14,4 +14,22 @@ public class Author {
     public String getAuthorSurname() {
         return this.authorSurname;
     }
+
+    @Override
+    public String toString() {
+        return " " + getAuthorName() + " " + getAuthorSurname();
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(getAuthorSurname(), getAuthorName());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return authorName.equals(author.authorName) && authorSurname.equals(author.authorSurname);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,17 @@ public class Main {
         Author konstantinSimonov = new Author("Konstantin","Simonov");
         Book harryPotter = new Book(joanRowling,"Harry Potter",2001);
         Book alivesAndDeads = new Book(konstantinSimonov, "Живые и мертвые", 1959);
+        Book alivesAndDeads2 = new Book(konstantinSimonov, "Живые и мертвые", 2004);
         harryPotter.setPublishingYear(1999);
         alivesAndDeads.setPublishingYear(1970);
         System.out.println("First Book of collection: " + harryPotter.getTitle() + " by " + harryPotter.getAuthor().getAuthorName()+ " " + harryPotter.getAuthor().getAuthorSurname()+ " published in "+ harryPotter.getPublishingYear());
         System.out.println("Second Book of collection: " + alivesAndDeads.getTitle() + " by " + alivesAndDeads.getAuthor().getAuthorName()+" " + alivesAndDeads.getAuthor().getAuthorSurname()+ " published in "+ alivesAndDeads.getPublishingYear());
-          }
+
+        System.out.println(harryPotter); //выведем результаты переопределенного метода toString()
+        System.out.println(alivesAndDeads);
+        System.out.println(alivesAndDeads.hashCode());  //выведем результаты переопределенного метода hashCode()
+        System.out.println(alivesAndDeads.equals(harryPotter)); //выведем результаты переопределенного метода equals()
+        System.out.println(alivesAndDeads2.equals(alivesAndDeads));
+    }
     
 }
